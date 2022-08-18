a30b309188110584a221e4ef254af898d45a7b62
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2022-08-18 16:33:30 -0700

    Start working out pw class

--- src/pw.java
+++ src/pw.java
@@ -1,3 +1,21 @@
+import java.security.SecureRandom;
+
 //for generating passwords
+public class pw {
 
+    public int[] randNums (int input) {
+        int[] array = new int[0];
 
+        SecureRandom random = new SecureRandom();
+
+        if (input > 0){
+            //TODO: use random to generate random numbers
+            //then use java file IO to get random entries from EFF long word list
+        } else {
+            array[0] = -1;
+        }
+        return array;
+
+    }
+
+}
